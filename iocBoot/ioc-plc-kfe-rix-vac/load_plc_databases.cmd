c5ab8af0f01b286de5f587359b2601b672280477
Jyoti Joshi <jjoshi@psbuild-rhel7-01.slac.stanford.edu>
2021-04-28 16:55:22 -0700

    Rebuild IOC with FS migration to same project

--- iocBoot/ioc-plc-kfe-rix-vac/load_plc_databases.cmd
+++ iocBoot/ioc-plc-kfe-rix-vac/load_plc_databases.cmd
@@ -1,1 +1,1 @@
-dbLoadRecords("plc_kfe_rix_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:RIX:VAC:,IOCNAME=$(IOCNAME)")
+dbLoadRecords("plc_kfe_rix_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:RIX:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
