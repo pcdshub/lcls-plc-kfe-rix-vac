574cdcc33cf01ddf04bde0dc4e97d047e7f2801e
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2021-06-09 15:44:36 -0700

    WIP: commit local work

--- iocBoot/ioc-plc-kfe-rix-vac/load_plc_databases.cmd
+++ iocBoot/ioc-plc-kfe-rix-vac/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 3906
-callbackSetQueueSize(9812)
+# Total records: 4094
+callbackSetQueueSize(10188)
 dbLoadRecords("plc_kfe_rix_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:RIX:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
--- iocBoot/ioc-plc-kfe-rix-vac/st.cmd
+++ iocBoot/ioc-plc-kfe-rix-vac/st.cmd
@@ -5,7 +5,7 @@
 
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
-epicsEnvSet("ENGINEER", "nwbrown" )
+epicsEnvSet("ENGINEER", "jjoshi" )
 epicsEnvSet("LOCATION", "PLC:KFE:RIX:VAC" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
