b85bf4641b658cca3dbfeb1aa4cf3fe34213e343
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2021-06-16 17:52:31 -0700

    BLD: rebuild

--- iocBoot/ioc-plc-kfe-rix-vac/load_plc_databases.cmd
+++ iocBoot/ioc-plc-kfe-rix-vac/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 4108
-callbackSetQueueSize(10216)
+# Total records: 4107
+callbackSetQueueSize(10214)
 dbLoadRecords("plc_kfe_rix_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:RIX:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
